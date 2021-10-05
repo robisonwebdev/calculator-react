02b2a97874268c06e2aa3c264e97ee2ba87b8bec
robisonwebdev <david@robisonweb.dev>
2021-10-05 11:44:40 -0600

    Setup module called calulatorButton that contain all buttons on calculator.

---
+++ src/modules/CalculatorButtons.js
@@ -0,0 +1,85 @@
+const calculatorButtons = [
+    {
+        name: 'clear',
+        button: 'C',
+    },
+    {
+        name: 'plusMinus',
+        button: '±'
+    },
+    {
+        name: 'percent',
+        button: '%'
+    },
+    {
+        name: 'divide',
+        button: '÷'
+    },
+    {
+        name: 'seven',
+        button: '7'
+    },
+    {
+        name: 'eight',
+        button: '8'
+    },
+    {
+        name: 'nine',
+        button: '9'
+    },
+    {
+        name: 'multiplication',
+        button: '×'
+    },
+    {
+        name: 'four',
+        button: '4'
+    },
+    {
+        name: 'five',
+        button: '5'
+    },
+    {
+        name: 'six',
+        button: '6'
+    },
+    {
+        name: 'plus',
+        button: '+'
+    },
+    {
+        name: 'one',
+        button: '1'
+    },
+    {
+        name: 'two',
+        button: '2'
+    },
+    {
+        name: 'three',
+        button: '3'
+    },
+    {
+        name: 'minus',
+        button: '-'
+    },
+    {
+        name: 'zero',
+        button: '0'
+    },
+    {
+        name: 'decimal',
+        button: '.'
+    },
+    {
+        name: 'backspace',
+        button: '←'
+    },
+    {
+        name: 'equal',
+        button: '='
+    }
+
+];
+
+module.exports = calculatorButtons;
