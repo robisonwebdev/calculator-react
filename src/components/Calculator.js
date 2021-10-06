30d24cb0d8d2419ee19f222c07ffe61edb09e1a3
robisonwebdev <david@robisonweb.dev>
2021-10-06 11:32:29 -0600

    Created function called handleOperator, to determine with switch statment which math operator to call.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import Display from './Display';
 import Controls from './Controls';
+import math from '../modules/MathOperations';
 import '../styles/Calculator.css';
 
 const Calculator = () => {
   const [calculatorButtons, setCalculatorButtons] = useState(require('../modules/CalculatorButtons'));
   const [display, setDisplay] = useState('0');
   const [operator, setOperator] = useState(null);
+  const [sum, setSum] = useState(0);
   const [valueOne, setValueOne] = useState(0);
   const [valueTwo, setValueTwo] = useState(0);
 
@@ -23,6 +25,23 @@ const Calculator = () => {
       console.log('No input!');
     }
     // setDisplay(input);
+  }
+
+  const handleOperator = (valueOne, operator, valueTwo) => {
+    switch (operator) {
+      case '+':
+        setSum(math.add(valueOne, valueTwo));
+        break;
+      case '-':
+        setSum(math.subtract(valueOne, valueTwo));
+        break;
+      case '*':
+        setSum(math.multiple(valueOne, valueTwo));
+        break;
+      case '/':
+        setSum(math.divide(valueOne, valueTwo));
+        break;
+    }
   }
 
   return (
