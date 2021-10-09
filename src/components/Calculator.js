75cff020d9d88877ebd170400c61859b1539f6a0
robisonwebdev <david@robisonweb.dev>
2021-10-09 08:13:16 -0600

    Removed setCalculatorButtons, since no changes to buttons will occur. Removed useState for operator. Removed useState for sum. Setup initial handleCalculation function. Current works on multiply, but doesn't useEffect isn't updating display. Changed all 'x' to '*'. Added if statement to '=' case in handleControlInput. Removed setSum from handleoperator function and replaced with returns.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,11 +6,9 @@ import '../styles/Calculator.css';
 
 const Calculator = () => {
   const [calculate, setCalculate] = useState(false);
-  const [calculatorButtons, setCalculatorButtons] = useState(require('../modules/CalculatorButtons'));
+  const [calculatorButtons] = useState(require('../modules/CalculatorButtons'));
   const [display, setDisplay] = useState();
-  const [operator, setOperator] = useState(null);
   const [storedValues, setStoredValues] = useState([0]);
-  const [sum, setSum] = useState(0);
 
   useEffect(() => {
     setDisplay(storedValues);
@@ -20,19 +18,36 @@ const Calculator = () => {
     console.log('Calculate: ', calculate);
   }, [calculate])
 
+  const handleCalculation = () => {
+    let newArray = storedValues;
+
+    while (storedValues.length !== 1) {
+      if (storedValues.findIndex(element => element === '*') !== -1) {
+        let operatorPosition = storedValues.findIndex(element => element === '*');
+        let newValue = handleOperator(parseFloat(storedValues[operatorPosition -1]), '*', parseFloat(storedValues[operatorPosition + 1]));
+
+        newArray.splice(operatorPosition -1, 3, newValue);
+      }
+    }
+    
+    setStoredValues(newArray);
+  }
+
   const handleControlInput = (value) => {
     const number = value.toString();
 
     switch (value) {
       case '+':
       case '-':
-      case '×':
+      case '*':
       case '/':
         setStoredValues(arr => [...arr, value]);
         setCalculate(false);
         break;
       case '=':
-        console.log('calculate', calculate);
+        if (calculate) {
+          handleCalculation();
+        }
         break;
       case 'clear':
         resetCalculator();
@@ -47,17 +62,13 @@ const Calculator = () => {
   const handleOperator = (valueOne, operator, valueTwo) => {
     switch (operator) {
       case '+':
-        setSum(math.add(valueOne, valueTwo));
-        break;
+        return math.add(valueOne, valueTwo);
       case '-':
-        setSum(math.subtract(valueOne, valueTwo));
-        break;
+        return math.subtract(valueOne, valueTwo);
       case '*':
-        setSum(math.multiple(valueOne, valueTwo));
-        break;
+        return math.multiple(valueOne, valueTwo);
       case '/':
-        setSum(math.divide(valueOne, valueTwo));
-        break;
+        return math.divide(valueOne, valueTwo);
     }
   }
 
@@ -73,7 +84,7 @@ const Calculator = () => {
     if (lastItem === 0) {
       newArray[0] = value;
       setStoredValues(newArray);
-    } else if (lastItem === '+' || lastItem === '-' || lastItem === '×' || lastItem === '/')  {
+    } else if (lastItem === '+' || lastItem === '-' || lastItem === '*' || lastItem === '/')  {
       setStoredValues(arr => [...arr, value]);
     } else {
       newArray[newArray.length - 1] = lastItem + value;
@@ -83,7 +94,6 @@ const Calculator = () => {
 
   return (
     <div className='calculator'>
-      {console.log(storedValues)}
       <Display
         display={display}
       />
