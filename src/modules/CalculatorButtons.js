c946c6dd5c4b1e8b9d1c8bdd77580846608ca9b9
robisonwebdev <david@robisonweb.dev>
2021-10-12 05:37:59 -0600

    Changed all number values to strings.

--- src/modules/CalculatorButtons.js
+++ src/modules/CalculatorButtons.js
@@ -20,17 +20,17 @@ const calculatorButtons = [
     {
         name: 'seven',
         button: '7',
-        value: 7
+        value: '7'
     },
     {
         name: 'eight',
         button: '8',
-        value: 8
+        value: '8'
     },
     {
         name: 'nine',
         button: '9',
-        value: 9
+        value: '9'
     },
     {
         name: 'times',
@@ -40,17 +40,17 @@ const calculatorButtons = [
     {
         name: 'four',
         button: '4',
-        value: 4
+        value: '4'
     },
     {
         name: 'five',
         button: '5',
-        value: 5
+        value: '5'
     },
     {
         name: 'six',
         button: '6',
-        value: 6
+        value: '6'
     },
     {
         name: 'plus',
@@ -60,17 +60,17 @@ const calculatorButtons = [
     {
         name: 'one',
         button: '1',
-        value: 1
+        value: '1'
     },
     {
         name: 'two',
         button: '2',
-        value: 2
+        value: '2'
     },
     {
         name: 'three',
         button: '3',
-        value: 3
+        value: '3'
     },
     {
         name: 'minus',
@@ -80,7 +80,7 @@ const calculatorButtons = [
     {
         name: 'zero',
         button: '0',
-        value: 0
+        value: '0'
     },
     {
         name: 'decimal',
