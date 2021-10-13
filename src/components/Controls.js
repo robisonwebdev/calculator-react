18f3c08cd118b3922c74bbb31dd359c8fcc23340
robisonwebdev <david@robisonweb.dev>
2021-10-13 11:17:58 -0600

    Changed handleControlInput to userInput.

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -2,8 +2,8 @@ import React from 'react';
 import Button from './Button';
 import '../styles/Controls.css';
 
-const Controls = ({ buttons, handleControlInput }) => {  
-  const placeButtons = buttons.map(button => <Button key={button.name} btn={button.button} onClick={handleControlInput} value={button.value} />)
+const Controls = ({ buttons, userInput }) => {  
+  const placeButtons = buttons.map(button => <Button key={button.name} btn={button.button} onClick={userInput} value={button.value} />)
 
   return (
     <div className='controls'>
