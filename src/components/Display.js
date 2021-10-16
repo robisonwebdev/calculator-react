82e5584b995c76ef0f4020eb4ef550d56bc1e211
robisonwebdev <david@robisonweb.dev>
2021-10-16 10:31:36 -0600

    Setup terney to check for input length being 1 and first character of string being '-'. Add bracket around string if true, and not around false.

--- src/components/Display.js
+++ src/components/Display.js
@@ -3,7 +3,9 @@ import '../styles/Display.css';
 
 const Display = ({ display }) => {
   let arrayInputs = [...display];
-  let displayArray = arrayInputs.map((input, index) => <p key={index}>{input}</p>)
+  let displayArray = arrayInputs.map((input, index) => {
+    return (input.charAt(0) === '-' && input.length !== 1) ? <p key={index}>({input})</p> : <p key={index}>{input}</p>;
+  })
 
   return (
     <div className='display'>
