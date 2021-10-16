3e0a6b16f8c0ff5b18ea4f2e14a1853b5d2a76e9
robisonwebdev <david@robisonweb.dev>
2021-10-16 10:13:57 -0600

    Created arrayInputs variable to make sure display props are showing as an array. Ran arrayInput through map, returning a <p>. Set to variable called displayArray. Called displayArray in return div.

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,9 +2,12 @@ import React from 'react';
 import '../styles/Display.css';
 
 const Display = ({ display }) => {
+  let arrayInputs = [...display];
+  let displayArray = arrayInputs.map((input, index) => <p key={index}>{input}</p>)
+
   return (
     <div className='display'>
-      {display}
+      {displayArray}
     </div>
   );
 }
