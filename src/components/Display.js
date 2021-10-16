d948eb5e37cec48b0ed0958432553a1802b3e388
robisonwebdev <david@robisonweb.dev>
2021-10-16 10:42:10 -0600

    Added if statements to change display if *, /, negative number, or single negative numbers are found. Adds better visual for calculator display.

--- src/components/Display.js
+++ src/components/Display.js
@@ -4,7 +4,28 @@ import '../styles/Display.css';
 const Display = ({ display }) => {
   let arrayInputs = [...display];
   let displayArray = arrayInputs.map((input, index) => {
-    return (input.charAt(0) === '-' && input.length !== 1) ? <p key={index}>({input})</p> : <p key={index}>{input}</p>;
+    // Puts no brackets around single negative number in array
+    if (arrayInputs.length === 1 && input.charAt(0) === '-') {
+      return <p key={index}>{input}</p>
+    }
+
+    // Puts bracket around negative number
+    if (input.charAt(0) === '-' && input.length !== 1) {
+      return <p key={index}>({input})</p>;
+    }
+
+    // Changes * to x
+    if (input === '*') {
+      return <p key={index}>x</p>;
+    }
+
+    // Changed / to ÷
+    if (input === '/') {
+      return <p key={index}>÷</p>
+    }
+
+    // Default return
+    return <p key={index}>{input}</p>
   })
 
   return (
